54a714d678b2466ce9f82ae996a33cd61c1f451e
Solias <92055766+SoliaSere@users.noreply.github.com>
2021-10-07 00:17:03 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+/*/
+This command was originally in the body of the index.html file, but according to the instructor, that is a beginners way to employ javascript.
+
+Also, this line is supossed to be at the beginning of the index.js page, but it's too advanced for my knoweldge level.
+
+
+document.getElementById("count-el").innerText = 5
+
+
+/*/
+
+
+
+
+
+/*/
+Note: Cannot put console.log command above the variable, 
+as javascript reads code from top to bottom
+
+ex:
+
+console.log(myAge)
+
+let myAge = 0
+
+result: Error cannot access 'myAge' before initialization
+
+
+/*/
+
+
+let myAge = 21
+
+console.log(myAge)
