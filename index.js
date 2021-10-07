f6cbd9aaaac5ab01563f886de92024b1b1e8db34
Solias <92055766+SoliaSere@users.noreply.github.com>
2021-10-07 19:52:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*/
+
 This command was originally in the body of the index.html file, but according to the instructor, that is a beginners way to employ javascript.
 
 Also, this line is supossed to be at the beginning of the index.js page, but it's too advanced for my knoweldge level.
@@ -7,14 +7,9 @@ Also, this line is supossed to be at the beginning of the index.js page, but it'
 document.getElementById("count-el").innerText = 5
 
 
-/*/
 
 
-
-
-
-/*/
-Note: Cannot put console.log command above the variable, 
+/*/Note: Cannot put console.log command above the variable, 
 as javascript reads code from top to bottom
 
 ex:
@@ -28,7 +23,38 @@ result: Error cannot access 'myAge' before initialization
 
 /*/
 
+//Defining Variables with Javascript//
+
 
 let myAge = 21
 
 console.log(myAge)
+
+
+
+
+//New exercise = Use javascript as a calculator between variables//
+
+
+let firstBatch = 5
+
+let secondBatch = 7
+
+let count = firstBatch + secondBatch
+
+console.log(count)
+
+
+//Console Answer was 12//
+
+
+
+let myAge = 21
+
+let humandogratio = 7
+
+let myDogAge = myAge * humandogratio
+
+console.log(myDogAge)
+
+//console answer was 147//
