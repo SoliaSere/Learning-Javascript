286d75f4b2bee5150f6ab9cb6fd83adca438381e
Solias <92055766+SoliaSere@users.noreply.github.com>
2021-10-07 20:22:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -58,3 +58,40 @@ let myDogAge = myAge * humandogratio
 console.log(myDogAge)
 
 //console answer was 147//
+
+
+
+
+//learning to reassign variables after already giving them a value//
+
+//As mentioned before, Javascript reads code from top to bottom
+//so giving a variable another value after already defining it 
+//will make the console log only see the one on the bottom
+
+let count = 5
+
+count = 3
+
+count = 1
+
+console.log(count)
+
+//console log response = 1
+
+
+
+//exercise: Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+
+
+let bonusPoints = 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
